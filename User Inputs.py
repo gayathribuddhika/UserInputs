fb054bad133c9125a0fd89e1fe972e36f7ef2380
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-28 21:28:18 +0530

    User Inputs
    User Inputs

---
+++ User Inputs.py
@@ -0,0 +1,16 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/Gayathri/Documents/python practicals/prac1.py ======
+Gayathri Buddhika
+>>> 
+====== RESTART: C:/Users/Gayathri/Documents/python practicals/prac1.py ======
+Gayathri Buddhika
+>>> x=10
+>>> x
+10
+>>> x=input("Enter the name:")
+Enter the name:Gayathri
+>>> print("My name is "+x)
+My name is Gayathri
+>>> 
